111c8b8211cd94cc3f4c9fd98d444fd0fa976ae6
Decklin Foster <decklin@red-bean.com>
2014-04-14 09:52:21 -0400

    If recv fails, do nothing and return 0

--- lib/net/ssh/buffered_io.rb
+++ lib/net/ssh/buffered_io.rb
@@ -62,9 +62,14 @@ module Net; module SSH
     # if no data was available to be read.
     def fill(n=8192)
       input.consume!
-      data = recv(n)
-      debug { "read #{data.length} bytes" }
-      input.append(data)
+      begin
+        data = recv(n)
+        debug { "read #{data.length} bytes" }
+        input.append(data)
+      rescue => e
+        data = ''
+        debug { "failed to read, #{e}" }
+      end
       return data.length
     end
 
